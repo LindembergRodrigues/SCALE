b822f5dc1078a0907efd8f9cd652bb30b9c58c71
Lindemberg Rodrigues <61949158+LindembergRodrigues@users.noreply.github.com>
2021-06-30 00:13:08 -0300

    Add files via upload

--- captura_dados.py
+++ captura_dados.py
@@ -7,21 +7,27 @@ Created on Tue Jun 29 05:29:12 2021
 """
 
 
+
 import requests
 import json
 from PIL import Image
 from io import BytesIO
 
-    
+#captura os dados na API
 requisicao_dos_dados = requests.get("https://reqres.in/api/users")
+# gera uma lista de dicionarios (achei melhor para ter acesso aos dados)
 lista_de_dados = json.loads(requisicao_dos_dados.content)
 
+
+#loop para acessar cada dado, imprimir nome, sobrenome e  email
 for dados in lista_de_dados["data"]:
     print("Nome: " + dados["first_name"]
     ,"\nSobrenome: " + dados["last_name"]
     ,"\nEmail: " + dados["email"]+"\n")
     
+    #requisição da imagen na API
     requisico_url_imagem = requests.get(dados["avatar"])
+    # registra a imagem na variável img, usando BytesIO() pela imagem se tratar de um binário
     img = Image.open(BytesIO(requisico_url_imagem.content))
+    #exibe a imagem
     img.show()
-    
