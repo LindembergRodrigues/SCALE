50d9f3b7f229371ebc6a18787beb6cc409f3d918
lindemberg <bergfla2010@hotmail.com>
2021-08-20 23:29:56 -0300

    Modificando para treinar os commits pelo terminal

--- captura_dados.py
+++ captura_dados.py
@@ -9,25 +9,24 @@ Created on Tue Jun 29 05:29:12 2021
 
 
 import requests
-import json
 from PIL import Image
 from io import BytesIO
 
 #captura os dados na API
 requisicao_dos_dados = requests.get("https://reqres.in/api/users")
-# gera uma lista de dicionarios (achei melhor para ter acesso aos dados)
-lista_de_dados = json.loads(requisicao_dos_dados.content)
 
+# dicionário do request 
+dados_request  = requisicao_dos_dados.json()
 
-#loop para acessar cada dado, imprimir nome, sobrenome e  email
-for dados in lista_de_dados["data"]:
-    print("Nome: " + dados["first_name"]
-    ,"\nSobrenome: " + dados["last_name"]
-    ,"\nEmail: " + dados["email"]+"\n")
-    
-    #requisição da imagen na API
-    requisico_url_imagem = requests.get(dados["avatar"])
-    # registra a imagem na variável img, usando BytesIO() pela imagem se tratar de um binário
-    img = Image.open(BytesIO(requisico_url_imagem.content))
-    #exibe a imagem
-    img.show()
+dados_usuarios = list(dados_request["data"])
+
+for dicionario  in dados_usuarios:
+    for key, value in dicionario.items():
+        if (key != "avatar"):
+            print(" Chave -> {} : Value -> {}".format(key, value))
+        else:
+            print(value)
+            img = Image.open(BytesIO(requests.get(value).content))
+            img.show()
+    print("\n Novo dicionário")
+   
